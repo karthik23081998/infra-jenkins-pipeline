273f7cd8fd08a613b7e469276d78e895d2a560fa
karthik23081998 <karthiksunny1211@gmail.com>
2025-10-28 23:32:14 +0530

    this jenkins

--- provider.tf
+++ provider.tf
@@ -1,19 +1,18 @@
-
-  terraform {
+terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.18.0"
     }
   }
 
-provider "aws" {
-    region = "ap-south-1"
-  
-}
-backend "s3" {
+  backend "s3" {
     bucket = "bhanu-2000-usa"
-    key = "jp/innfraterrafrom"
+    key    = "jp/innfraterrafrom"
     region = "ap-south-1"
-}
   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
